6e76ed03a32cc050c6bf3de96080a453cd2df6cc
1400092509 <1400092509@users.noreply.github.com>
2017-11-08 15:41:07 +0800

    增加了对年份的筛选

--- 批量处理.py
+++ 批量处理.py
@@ -19,13 +19,14 @@ csv_write=csv.writer(z,dialect='excel')
 
 for row in reader:
     list_now=[]
-    if row[3] != '0.0':
-        count += 1
-        if count>=0:
-            for i in range(0,15):
-                list_now.append(row[i])
-                # sum+=float(row[3])
-            csv_write.writerow(list_now)
-            print(reader.line_num,row)
+    # if row[3] != '0.0':
+    if row[0][0:4]=='2016':
+        # count += 1
+        # if count>=0:
+        for i in range(0,15):
+            list_now.append(row[i])
+            # sum+=float(row[3])
+        csv_write.writerow(list_now)
+        print(reader.line_num,row)
 print(row[0])
-print(type(row[0][0:4]))
+print(row[0][0:4])
